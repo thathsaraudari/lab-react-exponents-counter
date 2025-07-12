4b5f4a4390a212c44362e8ab0014ca9d9cb59cff
thathsaraudari <105314224+thathsaraudari@users.noreply.github.com>
2025-07-12 02:09:44 +0200

    done

--- src/components/ExponentFour.jsx
+++ src/components/ExponentFour.jsx
@@ -1,7 +1,7 @@
-const ExponentFour = () => (
+const ExponentFour = ({count}) => (
    <div className="exponent-counter-container">
     <p className="exponent-label">n⁴</p>
-    <p className="exponent-result">2 * 2 * 2 * 2 = <span className="total">16</span></p>
+    <p className="exponent-result">{count} * {count} * {count} * {count} = <span className="total">{Math.pow(count, 4)}</span></p>
   </div>
 );
 
--- src/components/ExponentTwo.jsx
+++ src/components/ExponentTwo.jsx
@@ -1,7 +1,7 @@
-const ExponentTwo = () => (
+const ExponentTwo = ({count}) => (
    <div className="exponent-counter-container">
     <p className="exponent-label">n²</p>
-    <p className="exponent-result">2 * 2 = <span className="total">4</span></p>
+    <p className="exponent-result">{count} * {count} = <span className="total">{Math.pow(count, 2)}</span></p>
   </div>
 );
 
